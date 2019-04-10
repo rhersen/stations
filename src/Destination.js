723fc3a8d3908f674356f19f3a79f45a0da94f9b
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-04-10 20:55:21 +0200

    Show station names

--- src/Destination.js
+++ src/Destination.js
@@ -1,9 +1,10 @@
 import React from "react"
+import ajax from "./ajax"
 
 export default function Destination({ announcement }) {
   return (
     <td className="destination">
-      {announcement.ToLocation.map(location => location.LocationName).join()}
+      {announcement.ToLocation.map(location => ajax.name(location.LocationName)).join()}
     </td>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from "react"
+import ajax from "./ajax"
 
 export default function Header({ location }) {
   return (
     <div>
       <span className="close">⇐</span>
-      <h1>{location}</h1>
+      <h1>{ajax.name(location)}</h1>
     </div>
   )
 }
