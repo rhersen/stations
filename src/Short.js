25a2c24d23a838e65066a5ebb3796f0acf0322ad
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-04-10 20:52:30 +0200

    Implement Short

--- src/Short.js
+++ src/Short.js
@@ -1,5 +1,9 @@
 import React from "react"
 
-export default function Short({ announcement }) {
-  return <td className="short">{announcement.Deviation && announcement.Deviation.join()}</td>
+export default function Short({ announcement: { Deviation } }) {
+  return <td className="short">{text(Deviation)}</td>
+}
+
+function text(a) {
+  return a && a.map(s => (/Kort/.test(s) ? "K" : s.length ? "*" : s))
 }
--- src/Short.test.js
+++ src/Short.test.js
@@ -1,11 +1,22 @@
-import { configure } from "enzyme"
+import { configure, shallow } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
-configure({ adapter: new Adapter() })
 import React from "react"
-import { shallow } from "enzyme"
 import Short from "./Short"
 
-test("class", () => {
-  const subject = shallow(<Short announcement={{}} />)
-  expect(subject.hasClass("short")).toBeTruthy()
+configure({ adapter: new Adapter() })
+
+test("text", () => {
+  expect(
+    shallow(<Short announcement={{ Deviation: ["Signalfel"] }} />).text()
+  ).toBe("*")
+  expect(
+    shallow(<Short announcement={{ Deviation: ["Kort tåg"] }} />).text()
+  ).toBe("K")
+  expect(shallow(<Short announcement={{ Deviation: [] }} />).text()).toBe("")
+  expect(shallow(<Short announcement={{}} />).text()).toBe("")
+  expect(
+    shallow(
+      <Short announcement={{ Deviation: ["Kort tåg", "Spårspring"] }} />
+    ).text()
+  ).toBe("K*")
 })
